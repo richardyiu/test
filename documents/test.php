376f86d9eb6ce2f8293c9cfac1ee199302b78638
intdev6 <intdev6@stradigidesign2-2.local>
2014-11-26 16:26:27 -0500

    infinite

---
+++ documents/test.php
@@ -0,0 +1,33 @@
+<?php 
+
+
+
+?>
+<html>
+<head>
+
+<style>
+	
+body{
+
+background: black;
+
+}	
+
+p{
+
+color: white;
+
+}
+
+</style>
+
+	<title></title>
+
+</head>
+
+<body>
+<p>hello</p>
+
+</body>
+</html>
